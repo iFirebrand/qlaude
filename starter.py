aef7fd164bd69c17ce3e2c5eefc2f271223ac652
Constantin <35843683+iFirebrand@users.noreply.github.com>
2026-02-11 17:43:39 -0500

    temp

--- starter.py
+++ starter.py
@@ -6,36 +6,56 @@ load_dotenv()
 
 client = anthropic.Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
 
+
 def add_user_message(messages, text):
     user_message = {"role": "user", "content": text}
     messages.append(user_message)
+
 
 def add_assistant_message(messages, text):
     assistant_message = {"role": "assistant", "content": text}
     messages.append(assistant_message)
 
-def chat(messages):
-    message = client.messages.create(
-        model="claude-haiku-4-5-20251001",
-        max_tokens=1024,
-        messages=messages,
-    )
-    # return message.content[0].text
+system_prompt = """ You are a patient math tutor. Do not directly answer students questions. Guide them to solutions step by step. """
+
+def chat(messages,system=None, temperature=1.0):
+    params = {
+        "model": "claude-haiku-4-5-20251001",
+        "max_tokens": 1024,
+        "messages": messages,
+        "temperature": temperature
+    }
+
+    if system:
+        params["system"] = system_prompt
+
+    message = client.messages.create(**params)
+    print(params)
     return message.content[0].text
 
-if __name__ == "__main__":
+def tutor():
     messages = []
-    # add initial question
-    add_user_message(messages, "Define bread making in one sentence")
-    # pass list of messages to chat
-    answer = chat(messages)
-    print(answer)
-    # take answer and add it as an assistant message to our list
-    add_assistant_message(messages, answer)
+    while True:
+        user_input = input("> ")
+        add_user_message(messages, user_input)
+        answer = chat(messages, system_prompt)
+        add_assistant_message(messages, answer)
+        print("🧑‍🎓" + answer)
 
-    # follow-up question using the conversation history
-    add_user_message(messages, "Now summarize that in 3 words")
-    answer = chat(messages)
-    print(answer)
-    print(messages)
+def interactive_chat():
+    messages = []
+    while True:
+        user_input = input("🤷‍♂️ ")
+        add_user_message(messages, user_input)
+        answer = chat(messages, temperature=1.0)
+        add_assistant_message(messages, answer)
+        print("---")
+        print(answer)
+        print("---")
 
+
+
+
+if __name__ == "__main__":
+    interactive_chat()
+    # tutor()
